b4c50f5bec2618be44f18b16c4872222fb2e0ba8
Emil Sauer Lynger <emillynge24@gmail.com>
2016-04-13 15:01:22 +0200

    add edit method to sms

--- plyer/platforms/android/sms.py
+++ plyer/platforms/android/sms.py
@@ -5,9 +5,10 @@ Android SMS
 
 from jnius import autoclass
 from plyer.facades import Sms
+from . import activity
 
 SmsManager = autoclass('android.telephony.SmsManager')
-
+Intent = autoclass('android.content.Intent')
 
 class AndroidSms(Sms):
 
@@ -19,6 +20,17 @@ class AndroidSms(Sms):
 
         if sms:
             sms.sendTextMessage(recipient, None, message, None, None)
+
+    def _edit(self, **kwargs):
+        intent = Intent(Intent.ACTION_VIEW)
+        intent.setType("vnd.android-dir/mms-sms")
+        if 'recipient' in kwargs:
+            intent.putExtra("address", str(kwargs.get('recipient')))
+
+        if 'message' in kwargs:
+            intent.putExtra("sms_body", str(kwargs.get('message')))
+
+        activity.startActivity(intent)
 
 
 def instance():
